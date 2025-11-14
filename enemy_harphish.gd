c431b840b001c390ea56c7de7cb7c120ffd869c0
Vakond <molnarzalan2008@gmail.com>
2025-11-14 21:18:59 +0100

    harphish rotation

--- enemy_harphish.gd
+++ enemy_harphish.gd
@@ -3,22 +3,29 @@ extends Node3D
 
 const ITEM := preload("uid://bmywilibhhjqb")
 const HARPHISH_BULLET := preload("uid://djsy4i2ta2uo2")
+
 @onready var shoot_cooldown : Timer= %ShootCooldown
+@onready var node : Node3D = Node3D.new()
+
 var tween = create_tween()
 
 func _ready() -> void:
 	await get_tree().create_timer(randi_range(-0.0,0.2)).timeout
 	shoot_cooldown.start()
-
+	add_sibling(node)
+	node.global_position = global_position
+	
 func _process(delta : float) -> void:
-	var dir = (Global.player.global_position - global_position).normalized()
-	#popipo
+	node.look_at(Global.player.global_position)
+	global_rotation = rotation.slerp(node.global_rotation,5.0 * delta) + Vector3()
+
 func _on_hitbox_died() -> void:
 	var item : RigidBody3D = ITEM.instantiate()
 	call_deferred("add_sibling",item)
 	item.global_position = global_position
 	item.angular_velocity = Vector3(randf_range(-1.0,1.0),randf_range(-1.0,1.0),randf_range(-1.0,1.0))
 	item.linear_velocity = (global_position - Global.player.global_position).normalized() * Vector3(5,0,5) + Vector3(0,5,0)
+	node.call_deferred("queue_free")
 	call_deferred("queue_free")
 	
 func _on_shoot_cooldown_timeout():
